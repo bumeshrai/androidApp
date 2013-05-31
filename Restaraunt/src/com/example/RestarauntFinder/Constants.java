6bfa712578557c424d9af6e8fe55c1404a3e314e
umesh <bumeshrai@hotmail.com>
2013-05-31 15:26:22 +0530

    added directory

---
+++ Restaraunt/src/com/example/RestarauntFinder/Constants.java
@@ -0,0 +1,12 @@
+package com.example.RestarauntFinder;
+
+public class Constants {
+
+    public static final String INTENT_ACTION_VIEW_DETAIL = "com.example.RestarauntFinder.VIEW_DETAIL";
+    public static final String INTENT_ACTION_VIEW_LIST = "com.example.RestarauntFinder.VIEW_LIST";
+
+    public static final String LOGTAG = "RestaurantFinder";
+    public static final String PREFS = "RCP";
+
+    public static final String STARTFROM_EXTRA = "com.example.RestarauntFinder.StartFrom";
+}
